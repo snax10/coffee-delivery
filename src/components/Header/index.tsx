9550e268508e677b34ccf13245c258b2745ee7b5
carlos.esilva <carlos.esilva@localiza.com>
2022-12-13 09:32:09 -0300

    feat: add component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,37 @@
-
-import { HeaderContainer } from './styles'
-import logoIgnite from '../../assets/coffe-delivery-logo.svg'
+import {
+    HeaderButton,
+    HeaderButtonsContainer,
+    HeaderContainer,
+} from "./styles";
+import coffeeLogoImg from "../../assets/coffe-delivery-logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
+
     return (
         <HeaderContainer>
-            <div>
-                <img src={logoIgnite} alt="" />
+            <div className="container">
+                <NavLink to="/">
+                    <img src={coffeeLogoImg} alt="" />
+                </NavLink>
+
+                <HeaderButtonsContainer>
+
+                    <HeaderButton variant="purple">
+                        <MapPin size={20} weight="fill" />
+                        Porto Alegre, RS
+                    </HeaderButton>
+
+                    <NavLink to="/completeOrder">
+                        <HeaderButton variant="yellow">
+                            <span>4</span>
+                            <ShoppingCart size={20} weight="fill" />
+                        </HeaderButton>
+                    </NavLink>
+
+                </HeaderButtonsContainer>
             </div>
         </HeaderContainer>
-    )
+    );
 }

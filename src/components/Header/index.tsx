00de8dcb13cf9b35cf38ba637e8796fbb4b71a36
carlos.esilva <carlos.esilva@localiza.com>
2022-12-13 09:06:14 -0300

    feat: add react router dom and routes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
-export function header() {
+
+import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/coffe-delivery-logo.svg'
+
+export function Header() {
     return (
-        <header>
-            <h1>Hello World</h1>
-        </header>
+        <HeaderContainer>
+            <div>
+                <img src={logoIgnite} alt="" />
+            </div>
+        </HeaderContainer>
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,3 @@
 import styled from "styled-components";
 
-export const Container = styled.header``;
+export const HeaderContainer = styled.header``;
